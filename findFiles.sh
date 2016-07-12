7b9e1876298a7e49033ca3c3bfdbde8e28279972
eddywebs <aditya@eddywebs.com>
2016-07-12 19:30:09 +0000

    updated findfiles script

--- findFiles.sh
+++ findFiles.sh
@@ -2,3 +2,6 @@
 #find files copyright files and copy them
 find /usr/share/doc -type f -name "*copyright*" -exec rsync -av -R {} ./ \;
 #find all the license.html and .txt files 
+sudo find / -type f -name "license.txt" -exec rsync -av -R {} ./ \;
+sudo find / -type f -name "license.html" -exec rsync -av -R {} ./ \;
+echo "check and delete any /home/ recursively added by the script, all done goodbye"
